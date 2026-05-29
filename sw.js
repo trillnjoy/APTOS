dbdf34545fdcefe732f1120b9f9c21a075fc29c1
trillnjoy <trillnjoy@icloud.com>
2026-05-29 16:17:20 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // APTOS Service Worker
 // Bump CACHE_VERSION to match APP_VERSION in index.html on every deploy
-const CACHE_VERSION = "1.2";
+const CACHE_VERSION = "1.3";
 const CACHE_NAME    = `aptos-v${CACHE_VERSION}`;
 
 const PRECACHE_URLS = [
